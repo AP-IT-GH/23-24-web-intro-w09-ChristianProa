357462cefe1f79c74caa15270cf3f723198b4e7d
ChristianProa <christoforoo@gmail.com>
2024-04-28 23:33:28 +0200

    versie 2

--- oefening-01-02/js/mapScript.js
+++ oefening-01-02/js/mapScript.js
@@ -2,14 +2,25 @@
 * Gebruik hiervoor de documentatie op https://leafletjs.com/ 
 * Verander in de code online alle "var" in "let".
 */
+document.addEventListener('DOMContentLoaded', function() {
+    // Maak de kaart aan en stel het centrum en zoomniveau in
+    var map = L.map('apMap').setView([51.23009, 4.41616], 17);
 
+    // Voeg OpenStreetMap tegels toe
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 
+    // Voeg een marker toe op de gegeven locatie
+    var marker = L.marker([51.23009, 4.41616]).addTo(map);
+    marker.bindPopup('<b>AP-Hogeschool</b><br>Ellermanstraat 33').openPopup();
 
-
-
-
-
-
+    // Definieer de grenzen van de rechthoek en voeg deze toe aan de kaart
+    var bounds = [[51.23041, 4.4155], [51.22991, 4.41675]];
+    var rectangle = L.rectangle(bounds, {color: "#FF0000", weight: 1}).addTo(map);
+    rectangle.bindPopup('Campus Ellerman');
+});
 
 
 
